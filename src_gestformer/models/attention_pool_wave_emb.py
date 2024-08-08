ea93936101f96fb9b0c93afd6017d0b2d82fd6bf
mallikagarg <mallika@ec.iitr.ac.in>
2024-08-08 13:20:42 +0530

    Initial commit

---
+++ src_gestformer/models/attention_pool_wave_emb.py
@@ -0,0 +1,270 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from einops import rearrange, repeat
+
+from pytorch_wavelets import DWTForward, DWTInverse
+
+def position_embedding(input, d_model):
+    input = input.view(-1, 1)
+    dim = torch.arange(d_model // 2, dtype=torch.float32, device=input.device).view(1, -1)
+    sin = torch.sin(input / 10000 ** (2 * dim / d_model))
+    cos = torch.cos(input / 10000 ** (2 * dim / d_model))
+
+    out = torch.zeros((input.shape[0], d_model), device=input.device)
+    out[:, ::2] = sin
+    out[:, 1::2] = cos
+    return out
+
+def sinusoid_encoding_table(max_len, d_model):
+    pos = torch.arange(max_len, dtype=torch.float32)
+    out = position_embedding(pos, d_model)
+    return out
+
+class ScaledDotProductAttention(nn.Module):
+    """
+    Scaled dot-product attention
+    """
+
+    def __init__(self, d_model, d_k, d_v, h):
+        """
+        :param d_model: Output dimensionality of the model
+        :param d_k: Dimensionality of queries and keys
+        :param d_v: Dimensionality of values
+        :param h: Number of heads
+        """
+        super(ScaledDotProductAttention, self).__init__()
+        self.fc_q = nn.Linear(d_model, h * d_k)
+        self.fc_k = nn.Linear(d_model, h * d_k)
+        self.fc_v = nn.Linear(d_model, h * d_v)
+        self.fc_o = nn.Linear(h * d_v, d_model)
+
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.h = h
+
+        self.init_weights(gain=1.0)
+
+    def init_weights(self, gain=1.0):
+        nn.init.xavier_normal_(self.fc_q.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_k.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_v.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_o.weight, gain=gain)
+        nn.init.constant_(self.fc_q.bias, 0)
+        nn.init.constant_(self.fc_k.bias, 0)
+        nn.init.constant_(self.fc_v.bias, 0)
+        nn.init.constant_(self.fc_o.bias, 0)
+
+    def forward(self, queries, keys, values):
+        """
+        Computes
+        :param queries: Queries (b_s, nq, d_model)
+        :param keys: Keys (b_s, nk, d_model)
+        :param values: Values (b_s, nk, d_model)
+        :return:
+        """
+        b_s, nq = queries.shape[:2]
+        nk = keys.shape[1]
+        q = self.fc_q(queries).view(b_s, nq, self.h, self.d_k).permute(0, 2, 1, 3)  # (b_s, h, nq, d_k)
+        k = self.fc_k(keys).view(b_s, nk, self.h, self.d_k).permute(0, 2, 3, 1)  # (b_s, h, d_k, nk)
+        v = self.fc_v(values).view(b_s, nk, self.h, self.d_v).permute(0, 2, 1, 3)  # (b_s, h, nk, d_v)
+
+        att = torch.matmul(q, k) / np.sqrt(self.d_k)  # (b_s, h, nq, nk)
+
+        att = torch.softmax(att, -1)
+
+        out = torch.matmul(att, v).permute(0, 2, 1, 3).contiguous().view(b_s, nq, self.h * self.d_v)  # (b_s, nq, h*d_v)
+        out = self.fc_o(out)  # (b_s, nq, d_model)
+        return out
+
+class MultiHeadAttention(nn.Module):
+    """
+    Multi-head attention layer with Dropout and Layer Normalization.
+    """
+
+    def __init__(self, d_model, d_k, d_v, h, dff=2048, dropout=.1):
+        super(MultiHeadAttention, self).__init__()
+
+        # self.attention = ScaledDotProductAttention(d_model=d_model, d_k=d_k, d_v=d_v, h=h)
+        self.dropout = nn.Dropout(p=dropout)
+        self.layer_norm = nn.LayerNorm(d_model)
+        # self.layer_norm2 = nn.LayerNorm(d_model)
+
+        self.fc = nn.Sequential(*[nn.Linear(d_model, dff), nn.ReLU(inplace=True), nn.Dropout(p=dropout),
+                                  nn.Linear(dff, d_model)])
+
+    def forward(self, queries, keys, values):
+        att = self.attention(queries, keys, values)
+        att = self.dropout(att)
+        # att = self.layer_norm(queries + att)
+        att = self.fc(att)
+        att = self.dropout(att)
+        return self.layer_norm(queries + att)
+
+class ScaledDotProductAttention_(nn.Module):
+    """
+    Scaled dot-product attention
+    """
+
+    def __init__(self, d_model, d_k, d_v, h):
+        """
+        :param d_model: Output dimensionality of the model
+        :param d_k: Dimensionality of queries and keys
+        :param d_v: Dimensionality of values
+        :param h: Number of heads
+        """
+        super(ScaledDotProductAttention_, self).__init__()
+        # print(d_model)
+        self.fc_q = nn.Linear(d_model, h * d_k)
+        self.fc_k = nn.Linear(d_model, h * d_k)
+        self.fc_v = nn.Linear(d_model, h * d_v)
+        self.fc_o = nn.Linear(h * d_v, d_model)
+
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.h = h
+        
+
+        self.init_weights(gain=1.0)
+
+    def init_weights(self, gain=1.0):
+        nn.init.xavier_normal_(self.fc_q.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_k.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_v.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_o.weight, gain=gain)
+        nn.init.constant_(self.fc_q.bias, 0)
+        nn.init.constant_(self.fc_k.bias, 0)
+        nn.init.constant_(self.fc_v.bias, 0)
+        nn.init.constant_(self.fc_o.bias, 0)
+
+    def forward(self, queries, keys, values):
+        """
+        Computes
+        :param queries: Queries (b_s, nq, d_model)
+        :param keys: Keys (b_s, nk, d_model)
+        :param values: Values (b_s, nk, d_model)
+        :return:
+        """
+        b_s, nq = queries.shape[:2]
+        # nk = keys.shape[1]
+        # print(queries.shape)
+        # print(b_s)
+        q = self.fc_q(queries).view(b_s,  self.h, self.d_k).permute(0, 1, 2)  # (b_s, h, nq, d_k)
+        k = self.fc_k(keys).view(b_s,  self.h, self.d_k).permute(0, 2,1)  # (b_s, h, d_k, nk)
+        v = self.fc_v(values).view(b_s,  self.h, self.d_v).permute(0, 1,  2)  # (b_s, h, nk, d_v)
+
+        att = torch.matmul(q, k) / np.sqrt(self.d_k)  # (b_s, h, nq, nk)
+
+        att = torch.softmax(att, -1)
+        
+
+        out = torch.matmul(att, v).permute(0, 1,2).contiguous().view(b_s, self.h * self.d_v)  # (b_s, nq, h*d_v)
+        out = self.fc_o(out)  # (b_s, nq, d_model)
+        return out
+
+class Pooling(nn.Module):
+    """
+    Implementation of pooling for PoolFormer
+    --pool_size: pooling size
+    """
+    def __init__(self, pool_size=3):
+        super().__init__()
+        self.pool = nn.AvgPool1d(
+            pool_size, stride=1, padding=pool_size//2, count_include_pad=False)
+
+    def forward(self, x):
+        return self.pool(x) 
+
+class SSL(nn.Module): 
+    def __init__(self, channels):
+        super(SSL, self).__init__()
+
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups=channels, bias=False)#conv_block_my(channels, channels, kernel_size = 3, stride=1, padding = 1, dilation=1)
+        self.conv5 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups=channels, bias=False)#conv_block_my(channels, channels, kernel_size = 3, stride=1, padding = 5, dilation=5)
+        self.conv7 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups=channels, bias=False)#conv_block_my(channels, channels, kernel_size = 3, stride=1, padding = 7, dilation=7)
+        self.conv9 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups=channels, bias=False)#conv_block_my(channels, channels, kernel_size = 3, stride=1, padding = 9, dilation=9)
+
+        self.conv_cat = nn.Conv2d(channels*4, channels, kernel_size=3, padding=1, groups=channels, bias=False)#conv_block_my(channels*4, channels, kernel_size = 3, stride = 1, padding = 1, dilation=1)
+
+    def forward(self, x):
+
+        aa =  DWTForward(J=1, mode='zero', wave='db3').cuda(device=2)
+        yl, yh = aa(x)
+
+        yh_out = yh[0]
+        ylh = yh_out[:,:,0,:,:]
+        yhl = yh_out[:,:,1,:,:]
+        yhh = yh_out[:,:,2,:,:]
+
+        conv_rec1 = self.conv5(yl)
+        conv_rec5 = self.conv5(ylh)
+        conv_rec7 = self.conv7(yhl)
+        conv_rec9 = self.conv9(yhh)
+
+        cat_all = torch.stack((conv_rec5, conv_rec7, conv_rec9),dim=2)
+        rec_yh = []
+        rec_yh.append(cat_all)
+
+
+        ifm = DWTInverse(wave='db3', mode='zero').cuda(device=2)
+        Y = ifm((conv_rec1, rec_yh))
+
+        return Y
+    
+class MultiHeadAttention_(nn.Module):
+    """
+    Multi-head attention layer with Dropout and Layer Normalization.
+    """
+
+    def __init__(self, d_model, d_k, d_v, h, dff=2048, dropout=.1):
+        super(MultiHeadAttention_, self).__init__()
+
+        # self.attention = ScaledDotProductAttention_(d_model=d_model, d_k=d_k, d_v=d_v, h=h)
+        self.s = SSL(1)
+        self.token_mixer = Pooling(pool_size=3)
+        self.dropout = nn.Dropout(p=dropout)
+        self.layer_norm = nn.LayerNorm(d_model)
+        # self.layer_norm2 = nn.LayerNorm(d_model)
+
+        self.fc = nn.Sequential(*[nn.Linear(d_model, dff), nn.ReLU(inplace=True), nn.Dropout(p=dropout),
+                                  nn.Linear(dff, d_model)])
+
+    # def forward(self, queries, keys, values):
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        x = self.s(x)
+        # print(x.shape)
+        x = x.squeeze(1)
+        att = self.token_mixer(x)
+        # print(att.shape)
+        # att = self.attention(queries, keys, values)
+        att = self.dropout(att)
+        # att = self.layer_norm(queries + att)
+        att = self.fc(att)
+        att = self.dropout(att)
+        return self.layer_norm( x+att)
+        
+class EncoderSelfAttention(nn.Module):
+    def __init__(self, d_model, d_k, d_v, n_head, dff=2048, dropout_transformer=.1, n_module=6):
+        super(EncoderSelfAttention, self).__init__()
+        # self.Spatial_pos_embed = nn.Parameter(torch.zeros(1, num_joints, embed_dim_ratio))
+        self.Spatial_embedding= nn.Linear(d_model, d_model)
+
+        # self.spatial_encoder = nn.ModuleList([MultiHeadAttention_(d_model*4, d_k*4, d_v*4, n_head, dff, dropout_transformer)
+        #                               for _ in range(n_module)])
+        # self.weighted_mean = torch.nn.Conv1d(in_channels=d_model*4, out_channels=d_model, kernel_size=1)
+
+        self.encoder = nn.ModuleList([MultiHeadAttention_(d_model, d_k, d_v, n_head, dff, dropout_transformer)
+                                      for _ in range(n_module)])
+                             
+
+    def forward(self, x):
+        x = self.Spatial_embedding(x)
+        in_encoder = x + sinusoid_encoding_table(x.shape[1], x.shape[2]).expand(x.shape).cuda(device=2)
+        for l in self.encoder:
+            # in_encoder = l(in_encoder, in_encoder, in_encoder)
+            in_encoder = l(in_encoder)
+        # print(in_encoder.shape)  # 8,40,512
+        return in_encoder
---
+++ src_gestformer/utils/test.py
@@ -0,0 +1,189 @@
+import numpy as np
+import torch
+import csv
+import pandas as pd
+
+from torch.utils.data import DataLoader
+import imgaug.augmenters as iaa
+
+# Import Datasets
+from datasets.Briareo import Briareo
+from datasets.NVGestures import NVGesture
+from models.model_utilizer import ModuleUtilizer
+
+# Import Model
+from models.temporal import GestureTransoformer
+
+# Import Utils
+from tqdm import tqdm
+from utils.average_meter import AverageMeter
+
+# Setting seeds
+
+from torchstat import stat
+def worker_init_fn(worker_id):
+    np.random.seed(torch.initial_seed() % 2 ** 32)
+
+class GestureTest(object):
+    """Gesture Recognition Test class
+
+    Attributes:
+        configer (Configer): Configer object, contains procedure configuration.
+        train_loader (torch.utils.data.DataLoader): Train data loader variable
+        val_loader (torch.utils.data.DataLoader): Val data loader variable
+        test_loader (torch.utils.data.DataLoader): Test data loader variable
+        net (torch.nn.Module): Network used for the current procedure
+        lr (int): Learning rate value
+        optimizer (torch.nn.optim.optimizer): Optimizer for training procedure
+        iters (int): Starting iteration number, not zero if resuming training
+        epoch (int): Starting epoch number, not zero if resuming training
+        scheduler (torch.optim.lr_scheduler): Scheduler to utilize during training
+
+    """
+
+    def __init__(self, configer):
+        self.configer = configer
+
+        self.data_path = configer.get("data", "data_path")      #: str: Path to data directory
+
+        # Train val and test accuracy
+        self.accuracy = AverageMeter()
+
+        # DataLoaders
+        self.data_loader = None
+
+        # Module load and save utility
+        self.device = self.configer.get("device")
+        self.model_utility = ModuleUtilizer(self.configer)      #: Model utility for load, save and update optimizer
+        self.net = None
+
+        # Training procedure
+        self.transforms = None
+
+        # Other useful data
+        self.backbone = self.configer.get("network", "backbone")     #: str: Backbone type
+        self.in_planes = None                                       #: int: Input channels
+        self.clip_length = self.configer.get("data", "n_frames")    #: int: Number of frames per sequence
+        self.n_classes = self.configer.get("data", "n_classes")     #: int: Total number of classes for dataset
+        self.data_type = self.configer.get("data", "type")          #: str: Type of data (rgb, depth, ir, leapmotion)
+        self.dataset = self.configer.get("dataset").lower()         #: str: Type of dataset
+        self.optical_flow = self.configer.get("data", "optical_flow")
+        if self.optical_flow is None:
+            self.optical_flow = True
+
+    def init_model(self):
+        """Initialize model and other data for procedure"""
+
+        if self.optical_flow is True:
+            self.in_planes = 2
+        elif self.data_type in ["depth", "ir"]:
+            self.in_planes = 1
+        else:
+            self.in_planes = 3
+
+        # Selecting correct model and normalization variable based on type variable
+        self.net = GestureTransoformer(self.backbone, self.in_planes, self.n_classes,
+                                       pretrained=self.configer.get("network", "pretrained"),
+                                       n_head=self.configer.get("network", "n_head"),
+                                       dropout_backbone=self.configer.get("network", "dropout2d"),
+                                       dropout_transformer=self.configer.get("network", "dropout1d"),
+                                       dff=self.configer.get("network", "ff_size"),
+                                       n_module=self.configer.get("network", "n_module")
+                                       )
+
+        self.net, _, _, _ = self.model_utility.load_net(self.net)
+
+        # Selecting Dataset and DataLoader
+        if self.dataset == "briareo":
+            Dataset = Briareo
+            self.transforms = iaa.CenterCropToFixedSize(200, 200)
+        elif self.dataset == "nvgestures":
+            Dataset = NVGesture
+            self.transforms = iaa.CenterCropToFixedSize(256, 192)
+        else:
+            raise NotImplementedError(f"Dataset not supported: {self.configer.get('dataset')}")
+
+        # Setting Dataloaders
+        self.data_loader = DataLoader(
+            Dataset(self.configer, self.data_path, split="val", data_type=self.data_type,
+                    transforms=self.transforms, n_frames=self.clip_length,
+                    optical_flow=self.optical_flow),
+            batch_size=1, shuffle=False, drop_last=True,
+            num_workers=self.configer.get('solver', 'workers'), pin_memory=True, worker_init_fn=worker_init_fn)
+
+    def __test(self):
+        """Testing function."""
+        self.net.eval()
+        # self.net.cuda()
+        num_params = sum(p.numel() for p in self.net.parameters())
+        print(f'Number of parameters in the model: {num_params}')
+        device = next(self.net.parameters()).device
+        print("device", device)
+        
+        if 'cuda' in str(device):
+            print("Model is on CUDA")
+        else:
+            print("Model is on CPU")
+        c = 0
+        tot = 0
+        with torch.no_grad():
+            with open('csv/Briareo/rgbop.csv', 'a', newline='') as csvfile:
+                for i, data_tuple in enumerate(tqdm(self.data_loader, desc="Test")):
+                    """
+                    input, gt
+                    """
+                    # self.device= torch.device('cpu')
+
+                    inputs = data_tuple[0].to(self.device)
+                    gt = data_tuple[1].to(self.device)
+
+                    output = self.net(inputs)
+                    # with torch.cuda.device(0):
+                    #     # print(inputs.shape.device)
+                    #     print(stat(self.net, inputs[0].shape))
+                    #print(output)
+                    #_, predicted = torch.max(output, 1)
+                    #print(predicted)
+
+                    sm = torch.nn.Softmax(dim=1)
+                    prob=sm(output)
+                    #print(i)
+                    predicted = torch.argmax(output.detach(), dim=1)
+                    #print(predicted)
+                    arr = prob.cpu().numpy()
+                    # print(arr)
+                    
+                    hist_df = pd.DataFrame(arr)
+                    # print(hist_df)
+                    #log_file.close()
+                    hist_df.to_csv(csvfile, mode='a',header=False, index=False)
+
+                    correct = gt.detach().squeeze(dim=1)
+                    #print(gt)
+                    #hist_df = pd.DataFrame(correct)
+                    #hist_df.to_csv(csvfile, mode='a',header=False)
+
+                    if predicted == correct:
+                        c += 1
+                    tot += 1
+
+            #print(predicted)
+            #print(correct)
+        accuracy = c / tot
+        #print(tot)
+
+        print("Accuracy: {}".format(accuracy))
+
+    def test(self):
+        self.__test()
+
+
+    def update_metrics(self, split: str, loss, bs, accuracy=None):
+        self.losses[split].update(loss, bs)
+        if accuracy is not None:
+            self.accuracy[split].update(accuracy, bs)
+        if split == "train" and self.iters % self.save_iters == 0:
+            self.tbx_summary.add_scalar('{}_loss'.format(split), self.losses[split].avg, self.iters)
+            self.tbx_summary.add_scalar('{}_accuracy'.format(split), self.accuracy[split].avg, self.iters)
+            self.losses[split].reset()
+            self.accuracy[split].reset()
